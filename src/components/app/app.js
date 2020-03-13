abc80956e476d9b07fc936340ceb3723ec1f1298
Kirill Ivanov <nfk23b@gmail.com>
2020-03-13 18:41:59 +0300

    initial commit

---
+++ src/components/app/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './app.css';
+
+const App = () => {
+    return (
+        <main role="main" className="container">
+            <div className="test">test</div>
+        </main>
+    )
+};
+
+export default App;
