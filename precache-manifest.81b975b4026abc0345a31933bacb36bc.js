2c8a7c24d78dd9ec045a60cb13d673b9b0ef5a94
nfk23b <nfk23b@gmail.com>
2020-03-30 23:12:54 +0300

    Updates

---
+++ precache-manifest.81b975b4026abc0345a31933bacb36bc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34ce9aaae0d0f802e769b468ef8250ae",
+    "url": "/react-movies-db/index.html"
+  },
+  {
+    "revision": "9141061986ad02af76da",
+    "url": "/react-movies-db/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "8ee0a29bf23d60af428e",
+    "url": "/react-movies-db/static/css/main.ecc39bcf.chunk.css"
+  },
+  {
+    "revision": "9141061986ad02af76da",
+    "url": "/react-movies-db/static/js/2.5206968b.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/react-movies-db/static/js/2.5206968b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ee0a29bf23d60af428e",
+    "url": "/react-movies-db/static/js/main.f85b3728.chunk.js"
+  },
+  {
+    "revision": "418ec8975bf656622927",
+    "url": "/react-movies-db/static/js/runtime-main.47d23567.js"
+  },
+  {
+    "revision": "2ee1c9c84a215da00217958e1745f691",
+    "url": "/react-movies-db/static/media/jay_and_silent_bob_reboot_canadian_movie_poster.2ee1c9c8.jpg"
+  }
+]);
