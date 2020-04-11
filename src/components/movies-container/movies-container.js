f58eff453263373c8e3cfa489d9055c53912f456
nfk23b <nfk23b@gmail.com>
2020-04-11 19:53:34 +0300

    gh_pages fix

--- src/components/movies-container/movies-container.js
+++ src/components/movies-container/movies-container.js
@@ -2,54 +2,51 @@ import React, { Component } from "react";
 import MoviesList from "../movies-list";
 import MovieInfo from "../movie-info";
 import { compose } from "../../utils";
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch } from "react-router-dom";
 import { Box } from "@material-ui/core";
 import { withStyles } from "@material-ui/styles";
 
-const styles = theme => ({
+const styles = (theme) => ({
     grid: {
-        backgroundColor: theme.palette.secondary.main
+        backgroundColor: theme.palette.secondary.main,
     },
     box: {
         paddingTop: theme.spacing(2),
-        paddingBottom: theme.spacing(5)
+        paddingBottom: theme.spacing(5),
     },
     root: {
         display: "flex",
         flexWrap: "wrap",
         justifyContent: "space-around",
         overflow: "hidden",
-        backgroundColor: theme.palette.background.paper
+        backgroundColor: theme.palette.background.paper,
     },
     icon: {
-        color: "rgba(255, 255, 255, 0.54)"
-    }
+        color: "rgba(255, 255, 255, 0.54)",
+    },
 });
 
 class MoviesContainer extends Component {
-    
     render() {
-        const {
-            classes
-        } = this.props;
+        const { classes } = this.props;
 
         return (
             <Box className={classes.box}>
                 <Switch>
-                    <Route 
-                        path="/"
+                    <Route
+                        path="/react-movies-db/"
                         component={MoviesList}
-                        exact/>
-                        
-                    <Route 
-                        path={`/movie/:id`}
-                        component={MovieInfo}/>
+                        exact
+                    />
+
+                    <Route
+                        path={`/react-movies-db/movie/:id`}
+                        component={MovieInfo}
+                    />
                 </Switch>
             </Box>
         );
     }
 }
 
-export default compose(
-    withStyles(styles),
-)(MoviesContainer);
+export default compose(withStyles(styles))(MoviesContainer);
